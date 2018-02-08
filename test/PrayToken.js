2a63f7724d2edef7b30ea015395e080fe12b74aa
cryptoprayer <dev@cryptoprayer.org>
2018-02-08 11:20:19 +0100

    Add mint test to PrayToken

--- test/PrayToken.js
+++ test/PrayToken.js
@@ -1,16 +1,22 @@
 const PrayToken = artifacts.require('./PrayToken.sol')
+
 contract('FundRaise', function ([owner, owner2]) {
   let token
 
   beforeEach('setup contract for each test', async function () {
-      token = await PrayToken.new(10*10**18)
+      token = await PrayToken.new(10*10**18);
   })
 
   it('has an owner', async function () {
-      assert.equal(await token.owner(), owner)
+      assert.equal(await token.owner(), owner);
   })
 
   it('has a cap set', async function () {
-      assert.equal(await token.cap(), 10*10**18)
+      assert.equal(await token.cap(), 10*10**18);
+  })
+
+  it('allows owner to mint', async function () {
+      await token.mint(owner2, 2*10**18);
+      assert.equal((await token.balanceOf(owner2)).toNumber(), 2*10**18);
   })
 })
