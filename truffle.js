66f0f174610e0b04a406c022a769d96025e2556b
cryptoprayer <dev@cryptoprayer.org>
2018-02-22 22:49:31 +0100

    Add ropsten to config

--- truffle.js
+++ truffle.js
@@ -16,7 +16,15 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id,
-
+    },
+    ropsten:  {
+      network_id: 3,
+      host: "localhost",
+      port:  8545,
+      gasPrice: 2000000000,
+      gas:   4000000,
+      from: '0xBb14e7dDDcEb3d09899B39bD3098e1EABaF5AA17'
     }
   }
 };
+
